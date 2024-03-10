8965c97d1dfa37ffa3caa01d280989b7b7d16627
Michael Schodin <michael.schodin@gmail.com>
2024-03-10 16:42:00 -0500

    updated shows

--- src/Components/Tour.js
+++ src/Components/Tour.js
@@ -2,43 +2,77 @@ import React from 'react';
 import './Tour.css';
 
 function Tour(props) {
+    let gigs = [
+        {
+            date: "March 30, 2024",
+            time: "",
+            venue: "Spinal Tap",
+            city: "Cedar Rapids, IA"
+        },
+        {
+            date: "April 28, 2024",
+            time: "5:00-8:00 PM",
+            venue: "Old Railroad Pub",
+            city: "Cedar Rapids, IA"
+        },
+        {
+            date: "May 17, 2024",
+            time: "7:00-10:00 PM",
+            venue: "The Jobsite",
+            city: "Iowa City, IA"
+        },
+        {
+            date: "May 24, 2024",
+            time: "1:40-2:00 PM",
+            venue: "KRFC Radio",
+            city: "Ft. Collins, CO"
+        },
+        {
+            date: "May 24, 2024",
+            time: "",
+            venue: "Little Bear Saloon",
+            city: "Evergreen, CO"
+        },
+        {
+            date: "May 25, 2024",
+            time: "8:00-11:00 PM",
+            venue: "Golden Moon Speakeasy",
+            city: "Golden, CO"
+        },
+        {
+            date: "May 26, 2024",
+            time: "8:00-10:00 PM",
+            venue: "East Fax Tap",
+            city: "Denver, CO"
+        }
+        ];
+
+    let gigsContent = [];
+
+    for (let i = 0; i < gigs.length; i++) {
+        let rowClassName = "";
+        if (i == 0) {
+            rowClassName = "tour-row-top";
+        } else if (i == gigs.length - 1) {
+            rowClassName = "tour-row-bottom";
+        } else {
+            rowClassName = "tour-row-mid";
+        }
+
+        gigsContent.push(
+            <div className={rowClassName}>
+                <div className="tour-date">{gigs[i].date} <a className="tour-at">at</a> {gigs[i].venue} <a className="tour-time">{gigs[i].time}</a></div>
+                <div className="tour-location">{gigs[i].city}</div>
+            </div>
+        );
+    }
+
     return (
         <>
             <div className="tour-content">
                 <h2 className='tour-table-header'>See Joytrip Live</h2>
                 <div className="tour-table">
-                    <div className="tour-row-top">
-                        <div className="tour-date">March 30, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> Spinal Tap</div>
-                        <div className="tour-location">Cedar Rapids, IA</div>
-                    </div>
-                    <div className="tour-row-mid">
-                        <div className="tour-date">April 28, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> Old Railroad Pub <a style={{fontStyle: "italic", fontWeight: "bold", fontSize:".8rem", marginRight: "4px"}}>5:00-8:00 PM</a></div>
-                        <div className="tour-location">Cedar Rapids, IA</div>
-                    </div>
-                    <div className="tour-row-mid">
-                        <div className="tour-date">May 17, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> Jobsite <a style={{fontStyle: "italic", fontWeight: "bold", fontSize:".8rem", marginRight: "4px"}}>7:00-10:00 PM</a></div>
-                        <div className="tour-location">Iowa City, IA</div>
-                    </div>
-                    <div className="tour-row-mid">
-                        <div className="tour-date">May 24, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> KRFC Radiol <a style={{fontStyle: "italic", fontWeight: "bold", fontSize:".8rem", marginRight: "4px"}}>1:40-2:00 PM</a></div>
-                        <div className="tour-location">Ft. Collins, CO</div>
-                    </div>
-                    <div className="tour-row-mid">
-                        <div className="tour-date">May 24, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> Little Bear Saloon</div>
-                        <div className="tour-location">Evergreen, CO</div>
-                    </div>
-                    <div className="tour-row-mid">
-                        <div className="tour-date">May 25, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> Trident Cafe <a style={{fontStyle: "italic", fontWeight: "bold", fontSize:".8rem", marginRight: "4px"}}>4:00-6:00 PM</a></div>
-                        <div className="tour-location">Boulder, CO</div>
-                    </div>
-                    <div className="tour-row-mid">
-                        <div className="tour-date">May 25, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> Golden Moon Speakeasy <a style={{fontStyle: "italic", fontWeight: "bold", fontSize:".8rem", marginRight: "4px"}}>8:00-11:00 PM</a></div>
-                        <div className="tour-location">Golden, CO</div>
-                    </div>
-                    <div className="tour-row-bottom">
-                        <div className="tour-date">May 26, 2024 <a style={{fontStyle: "italic", marginRight: "4px", fontSize: "1rem"}}>at</a> East Fax Tap <a style={{fontStyle: "italic", fontWeight: "bold", fontSize:".8rem", marginRight: "4px"}}>8:00-10:00 PM</a></div>
-                        <div className="tour-location">Denver, CO</div>
-                    </div>
+                    {gigsContent}
                 </div>
             </div>
         </>
